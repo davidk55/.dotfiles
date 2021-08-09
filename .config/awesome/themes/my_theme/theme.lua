7b47b0bbe60809b585b7935216037f22c2a21d13
David <david.programming@protonmail.com>
2021-08-10 00:45:48 +0200

    change to the right path

--- .config/awesome/themes/my_theme/theme.lua
+++ .config/awesome/themes/my_theme/theme.lua
@@ -32,24 +32,24 @@ theme.border_focus  = "#729fcf"
 theme.border_marked = "#eeeeec"
 
 -- IMAGES
-theme.layout_fairh           = themes_path .. "sky/layouts/fairh.png"
-theme.layout_fairv           = themes_path .. "sky/layouts/fairv.png"
-theme.layout_floating        = themes_path .. "sky/layouts/floating.png"
-theme.layout_magnifier       = themes_path .. "sky/layouts/magnifier.png"
-theme.layout_max             = themes_path .. "sky/layouts/max.png"
-theme.layout_fullscreen      = themes_path .. "sky/layouts/fullscreen.png"
-theme.layout_tilebottom      = themes_path .. "sky/layouts/tilebottom.png"
-theme.layout_tileleft        = themes_path .. "sky/layouts/tileleft.png"
-theme.layout_tile            = themes_path .. "sky/layouts/tile.png"
-theme.layout_tiletop         = themes_path .. "sky/layouts/tiletop.png"
-theme.layout_spiral          = themes_path .. "sky/layouts/spiral.png"
-theme.layout_dwindle         = themes_path .. "sky/layouts/dwindle.png"
-theme.layout_cornernw        = themes_path .. "sky/layouts/cornernw.png"
-theme.layout_cornerne        = themes_path .. "sky/layouts/cornerne.png"
-theme.layout_cornersw        = themes_path .. "sky/layouts/cornersw.png"
-theme.layout_cornerse        = themes_path .. "sky/layouts/cornerse.png"
+theme.layout_fairh           = themes_path .. "my_theme/layouts/fairh.png"
+theme.layout_fairv           = themes_path .. "my_theme/layouts/fairv.png"
+theme.layout_floating        = themes_path .. "my_theme/layouts/floating.png"
+theme.layout_magnifier       = themes_path .. "my_theme/layouts/magnifier.png"
+theme.layout_max             = themes_path .. "my_theme/layouts/max.png"
+theme.layout_fullscreen      = themes_path .. "my_theme/layouts/fullscreen.png"
+theme.layout_tilebottom      = themes_path .. "my_theme/layouts/tilebottom.png"
+theme.layout_tileleft        = themes_path .. "my_theme/layouts/tileleft.png"
+theme.layout_tile            = themes_path .. "my_theme/layouts/tile.png"
+theme.layout_tiletop         = themes_path .. "my_theme/layouts/tiletop.png"
+theme.layout_spiral          = themes_path .. "my_theme/layouts/spiral.png"
+theme.layout_dwindle         = themes_path .. "my_theme/layouts/dwindle.png"
+theme.layout_cornernw        = themes_path .. "my_theme/layouts/cornernw.png"
+theme.layout_cornerne        = themes_path .. "my_theme/layouts/cornerne.png"
+theme.layout_cornersw        = themes_path .. "my_theme/layouts/cornersw.png"
+theme.layout_cornerse        = themes_path .. "my_theme/layouts/cornerse.png"
 
-theme.awesome_icon           = themes_path .. "sky/awesome-icon.png"
+theme.awesome_icon           = themes_path .. "my_theme/awesome-icon.png"
 
 -- from default for now...
 theme.menu_submenu_icon     = themes_path .. "default/submenu.png"
@@ -64,7 +64,7 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 )
 
 -- MISC
-theme.wallpaper             = themes_path .. "sky/sky-background.png"
+theme.wallpaper             = themes_path .. "my_theme/sky-background.png"
 theme.taglist_squares       = "true"
 theme.titlebar_close_button = "true"
 theme.menu_height = dpi(15)
