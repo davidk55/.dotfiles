e94afa404d672dc46c928c5d2dddd516f9f63827
David <david.programming@protonmail.com>
2021-08-10 19:10:22 +0200

    change terminal to alacritty

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -48,7 +48,7 @@ end
 beautiful.init("/home/david/.config/awesome/themes/my_theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "st"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
