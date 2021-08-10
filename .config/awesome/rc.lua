19c1d1a126435e69696e34d09ee03d0163fd60d0
David <david.programming@protonmail.com>
2021-08-10 04:39:32 +0200

    add a rule for discord and obsidian

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -496,6 +496,10 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+     { rule = { class = "obsidian" },
+       properties = { tag = "4" } },
+     { rule = { instance = "discord" },
+       properties = { tag = "6" } },
 }
 -- }}}
 
