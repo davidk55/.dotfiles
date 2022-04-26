03500a550d55ddb575baae8980fd0d0fb5555c74
David <david.programming@protonmail.com>
2022-04-26 17:22:08 +0200

    change gitsigns mappings

--- .config/nvim/lua/plugins/setups/gitsigns.lua
+++ .config/nvim/lua/plugins/setups/gitsigns.lua
@@ -52,30 +52,29 @@ require("gitsigns").setup {
     end
 
     -- Navigation
-    map("n", "h]", function()
-      if vim.wo.diff then return "]c" end
+    map("n", ",j", function()
+      if vim.wo.diff then return ",j" end
       vim.schedule(function() gs.next_hunk() end)
       return "<Ignore>"
     end, {expr=true})
 
-    map("n", "h[", function()
-      if vim.wo.diff then return "[c" end
+    map("n", ",k", function()
+      if vim.wo.diff then return ",k" end
       vim.schedule(function() gs.prev_hunk() end)
       return "<Ignore>"
     end, {expr=true})
 
     -- Actions
-    map({"n", "v"}, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-    map({"n", "v"}, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-    map("n", "<leader>hS", gs.stage_buffer)
-    map("n", "<leader>hu", gs.undo_stage_hunk)
-    map("n", "<leader>hR", gs.reset_buffer)
-    map("n", "<leader>hp", gs.preview_hunk)
-    map("n", "<leader>hb", function() gs.blame_line{full=true} end)
-    map("n", "<leader>tb", gs.toggle_current_line_blame)
-    map("n", "<leader>hd", gs.diffthis)
-    map("n", "<leader>hb", function() gs.diffthis("~") end)
-    map("n", "<leader>td", gs.toggle_deleted)
+    map({"n", "v"}, ",s", ":Gitsigns stage_hunk<CR>")
+    map({"n", "v"}, ",r", ":Gitsigns reset_hunk<CR>")
+    map("n", ",S", gs.stage_buffer)
+    map("n", ",u", gs.undo_stage_hunk)
+    map("n", ",R", gs.reset_buffer)
+    map("n", ",p", gs.preview_hunk)
+    map("n", ",b", function() gs.blame_line{full=true} end)
+    map("n", ",tb", gs.toggle_current_line_blame)
+    map("n", ",td", gs.toggle_deleted)
+    map("n", ",D", function() gs.diffthis("~") end)
 
     -- Text object
     map({"o", "x"}, "ih", ":<C-U>Gitsigns select_hunk<CR>")
