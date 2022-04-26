dd47da01b9072bab2f04c6a62af9ad6ac538df6b
David <david.programming@protonmail.com>
2022-04-26 17:21:00 +0200

    change lsp mappings

--- .config/nvim/lua/plugins/lsp/lsp-mappings.lua
+++ .config/nvim/lua/plugins/lsp/lsp-mappings.lua
@@ -13,17 +13,17 @@ local on_attach = function(_,bufnr)
   local nsb = { noremap = true, silent = true, buffer = bufnr}
   vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", nsb)
   vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", nsb)
-  vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", nsb)
+  vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", nsb)
   vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", nsb)
+  vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", nsb)
   vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", nsb)
   vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", nsb)
   vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", nsb)
   vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", nsb)
   vim.keymap.set("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", nsb)
   vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", nsb)
-  vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", nsb)
-  vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", nsb)
-  vim.keymap.set("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", nsb)
+  vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", nsb)
+  vim.keymap.set("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", nsb)
 end
 
 return on_attach
