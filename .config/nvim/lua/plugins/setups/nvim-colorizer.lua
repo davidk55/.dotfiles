231c2827159e3fe27b1768d1fac65e54131e8765
David <david.programming@protonmail.com>
2022-05-27 15:47:31 +0200

    make hsl and rgb functions also show colors

--- .config/nvim/lua/plugins/setups/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/setups/nvim-colorizer.lua
@@ -1,8 +1,15 @@
 require"colorizer".setup{
-
   -- filetypes where this plugin is active
   "css",
   "javascript",
-  "html"
+  "html",
+  css = {
+    names = false,
+    RGB = true,
+    RRGGBB = true,
+    RRGGBBAA = true,
+    rgb_fn = true,
+    hsl_fn = true,
+  }
 
 }
