93f22bb66860e6af7b888541a5671fa50797dcbc
David <david.programming@protonmail.com>
2022-06-13 21:11:58 +0200

    change some lsp mappings

--- .config/nvim/lua/plugins/lsp/lsp-mappings.lua
+++ .config/nvim/lua/plugins/lsp/lsp-mappings.lua
@@ -1,8 +1,11 @@
 local ns = { noremap = true, silent = true}
-vim.keymap.set("n", "<leader>dd", "<cmd>lua vim.diagnostic.open_float()<CR>", ns)
 vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", ns)
 vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", ns)
-vim.keymap.set("n", "<leader>dl", "<cmd>lua vim.diagnostic.setloclist()<CR>", ns)
+vim.keymap.set("n", "<leader>dl", "<cmd>Trouble<CR>", ns)
+vim.keymap.set("n", "<leader>dd", "<cmd>Trouble document_diagnostics<CR>", ns)
+vim.keymap.set("n", "<leader>dw", "<cmd>Trouble workspace_diagnostics<CR>", ns)
+vim.keymap.set("n", "<leader>dl", "<cmd>Trouble loclist<CR>", ns)
+vim.keymap.set("n", "<leader>dq", "<cmd>Trouble quickfix<CR>", ns)
 
 local on_attach = function(_,bufnr)
   -- Enable completion triggered by <c-x><c-o>
@@ -12,18 +15,18 @@ local on_attach = function(_,bufnr)
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local nsb = { noremap = true, silent = true, buffer = bufnr}
   vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", nsb)
-  vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", nsb)
-  vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", nsb)
+  vim.keymap.set("n", "gd", "<cmd>Trouble lsp_definitions<CR>", nsb)
+  vim.keymap.set("n", "gr", "<cmd>Trouble lsp_references<CR>", ns)
   vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", nsb)
   vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", nsb)
   vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", nsb)
   vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", nsb)
   vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", nsb)
   vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", nsb)
-  vim.keymap.set("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", nsb)
-  vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", nsb)
-  vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", nsb)
-  vim.keymap.set("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", nsb)
+  vim.keymap.set("n", "<leader>D", "<cmd>Trouble lsp_type_definitions<CR>", nsb)
+  vim.keymap.set("n", "<leader>rn", "<cmd>lua require('lspsaga.rename').rename()<CR>", nsb)
+  vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", nsb)
+  vim.keymap.set("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", nsb)
 end
 
 return on_attach
