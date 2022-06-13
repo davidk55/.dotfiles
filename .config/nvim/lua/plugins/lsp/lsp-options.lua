fc770398a79fe298b7c2252973b10ce4f2163799
David <david.programming@protonmail.com>
2022-06-13 21:14:37 +0200

    outsource lsp options

---
+++ .config/nvim/lua/plugins/lsp/lsp-options.lua
@@ -0,0 +1,13 @@
+-- set signs
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- use circle instead of square
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●'
+  }
+})
