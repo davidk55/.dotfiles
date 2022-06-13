dbb4d7a82cb82313623aa691a39b23ad9b88367c
David <david.programming@protonmail.com>
2022-06-13 20:46:05 +0200

    change some mappings

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -76,29 +76,54 @@ vim.opt.shiftwidth = 4
 -- ********************** MAPPINGS **********************
 local ns = { noremap = true, silent = true}
 local nsb = { noremap = true, silent = true, buffer = 0}
+local e = { expr = true}
 -- Diagnostics
-vim.keymap.set("n", "<leader>dd", "<cmd>lua vim.diagnostic.open_float()<CR>", ns)
 vim.keymap.set("n", "d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", ns)
 vim.keymap.set("n", "d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", ns)
-vim.keymap.set("n", "<leader>dl", "<cmd>lua vim.diagnostic.setloclist()<CR>", ns)
+vim.keymap.set("n", "<leader>dl", "<cmd>Trouble<CR>", ns)
+vim.keymap.set("n", "<leader>dd", "<cmd>Trouble document_diagnostics<CR>", ns)
+vim.keymap.set("n", "<leader>dw", "<cmd>Trouble workspace_diagnostics<CR>", ns)
+vim.keymap.set("n", "<leader>dl", "<cmd>Trouble loclist<CR>", ns)
+vim.keymap.set("n", "<leader>dq", "<cmd>Trouble quickfix<CR>", ns)
 
--- Rest
+-- GENERAL
 vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", nsb)
-vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", nsb)
-vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", nsb)
+vim.keymap.set("n", "gd", "<cmd>Trouble lsp_definitions<CR>", nsb)
+vim.keymap.set("n", "gr", "<cmd>Trouble lsp_references<CR>", ns)
 vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", nsb)
 vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", nsb)
 vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", nsb)
 vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", nsb)
 vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", nsb)
 vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", nsb)
-vim.keymap.set("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", nsb)
-vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", nsb)
+vim.keymap.set("n", "<leader>D", "<cmd>Trouble lsp_type_definitions<CR>", nsb)
+vim.keymap.set("n", "<leader>rn", "<cmd>lua require('lspsaga.rename').rename()<CR>", nsb)
 vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", nsb)
 vim.keymap.set("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", nsb)
-vim.keymap.set("n", "<A-o>", "<cmd>lua require(\"jdtls\").organize_imports()<CR>", nsb)
-vim.keymap.set("n", "crv", "<cmd>lua require(\"jdtls\").extract_variable()<CR>", nsb)
-vim.keymap.set("v", "crv", "<esc><cmd>lua require(\"jdtls\").extract_variable(true)<CR>", nsb)
-vim.keymap.set("n", "crc", "<cmd>lua require(\"jdtls\").extract_constant()<CR>", nsb)
-vim.keymap.set("v", "crc", "<esc><cmd>lua require(\"jdtls\").extract_constant(true)<CR>", nsb)
-vim.keymap.set("v", "crm", "<esc><cmd>lua require(\"jdtls\").extract_method(true)<CR>", nsb)
+vim.keymap.set("n", "<leader>ri", "<cmd>lua require(\"jdtls\").organize_imports()<CR>", nsb)
+vim.keymap.set("n", "<leader>ev", "<cmd>lua require(\"jdtls\").extract_variable()<CR>", nsb)
+vim.keymap.set("v", "<leader>ev", "<esc><cmd>lua require(\"jdtls\").extract_variable(true)<CR>", nsb)
+vim.keymap.set("n", "<leader>ec", "<cmd>lua require(\"jdtls\").extract_constant()<CR>", nsb)
+vim.keymap.set("v", "<leader>ec", "<esc><cmd>lua require(\"jdtls\").extract_constant(true)<CR>", nsb)
+vim.keymap.set("v", "<leader>em", "<esc><cmd>lua require(\"jdtls\").extract_method(true)<CR>", nsb)
+
+-- CUSTOM
+vim.keymap.set("i", "<A-;>", "<esc>A;<CR>", nsb)
+vim.keymap.set("n", "<A-;>", function()
+  local line = vim.api.nvim_get_current_line()
+  if string.sub(line, #line, #line) == ";" then
+    return "$i"
+  else
+    return "A;<esc>^"
+  end
+end, e)
+vim.keymap.set("n", "<A-'>", function()
+  local line = vim.api.nvim_get_current_line()
+  if string.find(line, "System") ~= nil then
+    return "^df(<esc>$hx^"
+  elseif string.sub(line, #line, #line) == ";" then
+    return "ISystem.out.println(<esc>$i)<esc>^"
+  else
+    return "ISystem.out.println(<esc>$a);<esc>^"
+  end
+end, e)
