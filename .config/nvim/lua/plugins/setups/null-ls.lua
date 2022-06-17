f4676b6013734771bc962735378a8ec87778c95d
David <david.programming@protonmail.com>
2022-06-17 12:52:33 +0200

    use 2 spaces for indentation for stylua

--- .config/nvim/lua/plugins/setups/null-ls.lua
+++ .config/nvim/lua/plugins/setups/null-ls.lua
@@ -1,6 +1,13 @@
 require("null-ls").setup({
 	sources = {
-		require("null-ls").builtins.formatting.stylua,
+    require("null-ls").builtins.formatting.stylua.with({
+      extra_args = {
+        "--indent-type",
+        "Spaces",
+        "--indent-width",
+        "2",
+      },
+    }),
 		require("null-ls").builtins.formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" }}),
 		require("null-ls").builtins.formatting.google_java_format
 	},
