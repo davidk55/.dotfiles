a61d220a32ff8cf96aaaaec90e6b8e2d74b083ea
David <david.programming@protonmail.com>
2022-08-03 03:48:25 +0200

    replace more lsp mappings with lspsaga

--- .config/nvim/lua/plugins/lsp/lsp-mappings.lua
+++ .config/nvim/lua/plugins/lsp/lsp-mappings.lua
@@ -12,19 +12,28 @@ local on_attach = function(_, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local nsb = { noremap = true, silent = true, buffer = bufnr }
+  local action = require("lspsaga.action")
   vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", nsb)
-  vim.keymap.set("n", "gd", "<cmd>Trouble lsp_definitions<CR>", nsb)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, nsb)
+  vim.keymap.set("n", "<leader>ld", require("lspsaga.definition").preview_definition, ns)
   vim.keymap.set("n", "gr", "<cmd>Trouble lsp_references<CR>", ns)
   vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", nsb)
-  vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", nsb)
   vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", nsb)
   vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", nsb)
   vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", nsb)
   vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", nsb)
   vim.keymap.set("n", "<leader>D", "<cmd>Trouble lsp_type_definitions<CR>", nsb)
-  vim.keymap.set("n", "<leader>rn", "<cmd>lua require('lspsaga.rename').rename()<CR>", nsb)
-  vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", nsb)
+  vim.keymap.set("n", "<leader>rn", "<cmd>lua require('lspsaga.rename').lsp_rename()<CR>", nsb)
+  vim.keymap.set("n", "<leader>la", require("lspsaga.codeaction").code_action, nsb)
   vim.keymap.set("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", nsb)
+
+  vim.keymap.set("n", "K", require("lspsaga.hover").render_hover_doc, { silent = true })
+  vim.keymap.set("n", "<C-f>", function()
+      action.smart_scroll_with_saga(1)
+  end, { silent = true })
+  vim.keymap.set("n", "<C-b>", function()
+      action.smart_scroll_with_saga(-1)
+  end, { silent = true })
 end
 
 return on_attach
