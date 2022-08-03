0e15a6e81b2ed18d9d67b6c98af18d3b199aad61
David <david.programming@protonmail.com>
2022-08-03 21:13:16 +0200

    use pcall to prevent thrown error

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -2,7 +2,7 @@ local M = {}
 
 -- used to load from the plugin specific file in the setup directory
 function M.load_setup(plugin_name)
-  return string.format('require("plugins.setups.%s")', plugin_name)
+  pcall(require, "plugins.setups." .. plugin_name)
 end
 
 return M
