7f2e5b67b3ed01b4e8166a8a00d0024fd51b7a10
David <david.programming@protonmail.com>
2022-08-03 20:25:07 +0200

    auto format on save

--- .config/nvim/lua/plugins/setups/null-ls.lua
+++ .config/nvim/lua/plugins/setups/null-ls.lua
@@ -1,3 +1,4 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
   sources = {
     require("null-ls").builtins.formatting.stylua.with({
@@ -11,4 +12,17 @@ require("null-ls").setup({
     require("null-ls").builtins.formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
     require("null-ls").builtins.formatting.google_java_format,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
 })
