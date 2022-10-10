499102bec0b1ec4bc347a6ea8ce0af2cfe4ce736
David <david.programming@protonmail.com>
2022-10-10 03:16:01 +0200

    use treesitter server when its a dependency

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -5,4 +5,23 @@ function M.load_setup(plugin_name)
   pcall(require, "plugins.setups." .. plugin_name)
 end
 
+local function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then
+    f:close()
+  end
+  return f ~= nil
+end
+
+function M.get_lines(path)
+  if not file_exists(path) then
+    return {}
+  end
+  local lines = {}
+  for line in io.lines(path) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
 return M
