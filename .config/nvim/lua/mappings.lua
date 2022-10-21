d264c43ddb31415b8f38adeddedc24e216f0b097
David <david.programming@protonmail.com>
2022-10-21 18:01:21 +0200

    add smart-splits.nvim plugin

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -173,3 +173,16 @@ function _G.Toggle_venn()
 end
 -- toggle keymappings for venn using <leader>v
 vim.keymap.set("n", "<leader><leader>v", ":lua Toggle_venn()<CR>", { noremap = true })
+
+-- *************************** SMART-SPLITS ******************************
+-- resizing splits
+vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+
+-- moving between splits
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
