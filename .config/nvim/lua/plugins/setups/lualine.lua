a0600192d6f10ef8fb7f1c7b5473c67ffce27a12
David <david.programming@protonmail.com>
2022-11-16 17:47:07 +0100

    when recording show it in lualine

--- .config/nvim/lua/plugins/setups/lualine.lua
+++ .config/nvim/lua/plugins/setups/lualine.lua
@@ -1,6 +1,16 @@
 local navic = require("nvim-navic")
+local lualine = require("lualine")
 
-require("lualine").setup({
+local function show_macro_recording()
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == "" then
+    return ""
+  else
+    return "Recording @" .. recording_register
+  end
+end
+
+lualine.setup({
 
   options = {
     icons_enabled = true,
@@ -13,7 +23,15 @@ require("lualine").setup({
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_b = {
+      {
+        "macro-recording",
+        fmt = show_macro_recording,
+      },
+      "branch",
+      "diff",
+      "diagnostics",
+    },
     lualine_c = { "filename", { navic.get_location, cond = navic.is_available } },
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
@@ -29,4 +47,33 @@ require("lualine").setup({
   },
   tabline = {},
   extensions = {},
+})
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    lualine.refresh({
+      place = { "statusline" },
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    -- This is going to seem really weird!
+    -- Instead of just calling refresh we need to wait a moment because of the nature of
+    -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+    -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+    -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+    -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+    local timer = vim.loop.new_timer()
+    timer:start(
+      50,
+      0,
+      vim.schedule_wrap(function()
+        lualine.refresh({
+          place = { "statusline" },
+        })
+      end)
+    )
+  end,
 })
