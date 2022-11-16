7b4c048445900a7302f648ce22163e8086ac13ea
David <david.programming@protonmail.com>
2022-11-16 17:47:31 +0100

    add mapping to toggle lazydocker

--- .config/nvim/lua/plugins/setups/toggleterm.lua
+++ .config/nvim/lua/plugins/setups/toggleterm.lua
@@ -6,12 +6,17 @@ require("toggleterm").setup({
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = "float" })
 local node = Terminal:new({ cmd = "node", hidden = true, direction = "float", floatopts = { "curved" } })
 local lua = Terminal:new({ cmd = "lua", hidden = true, direction = "float" })
 local java = Terminal:new({ cmd = "jshell", hidden = true, direction = "float" })
 
 function _LAZYGIT_TOGGLE()
   lazygit:toggle()
+end
+
+function _LAZYDOCKER_TOGGLE()
+  lazydocker:toggle()
 end
 
 function _NODE_TOGGLE()
