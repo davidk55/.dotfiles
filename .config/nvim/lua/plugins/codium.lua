13ac23c1b806f685b428017dc130c1a559f5518c
David <david.programming@protonmail.com>
2023-02-10 10:53:50 +0100

    change codium mappings

--- .config/nvim/lua/plugins/codium.lua
+++ .config/nvim/lua/plugins/codium.lua
@@ -1,4 +1,19 @@
 return {
   "Exafunction/codeium.vim",
   ft = { "html", "css", "typescript", "typescriptreact", "javascript", "javascriptreact", "java" },
+  enable = false,
+  config = function()
+    vim.keymap.set("i", "<M-CR>", function()
+      return vim.fn["codeium#Accept"]()
+    end, { expr = true })
+    vim.keymap.set("i", "<M-]>", function()
+      return vim.fn["codeium#CycleCompletions"](1)
+    end, { expr = true })
+    vim.keymap.set("i", "<M-[>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true })
+    vim.keymap.set("i", "<M-BS>", function()
+      return vim.fn["codeium#Clear"]()
+    end, { expr = true })
+  end,
 }
