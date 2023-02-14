0b12b028a227bdc709eecb464cc9cf1cfa859a41
David <david.programming@protonmail.com>
2023-02-14 16:54:40 +0100

    add mapping to reload my testplugin

--- .config/nvim/lua/plugins/my-plugins.lua
+++ .config/nvim/lua/plugins/my-plugins.lua
@@ -2,6 +2,13 @@ return {
   {
     "davidk55/testplugin.nvim",
     dev = true, -- load from local path: ~/code/projects/nvim
-    enabled = false,
+    enabled = true,
+    config = function()
+      -- mapping to reload the plugin
+      vim.keymap.set("n", "<leader><leader>r", function()
+        local plugin = require("lazy.core.config").plugins["testplugin.nvim"]
+        require("lazy.core.loader").reload(plugin)
+      end, {})
+    end,
   },
 }
