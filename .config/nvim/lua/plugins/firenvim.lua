a9efad9e944dc31be6e67701900084edd4986c64
David <david.programming@protonmail.com>
2023-03-01 16:32:19 +0100

    fix firenvim error

--- .config/nvim/lua/plugins/firenvim.lua
+++ .config/nvim/lua/plugins/firenvim.lua
@@ -1,6 +1,7 @@
 return {
   "glacambre/firenvim",
   build = function()
+    require("lazy").load({ plugins = "firenvim", wait = true })
     vim.fn["firenvim#install"](0)
   end,
   cond = not not vim.g.started_by_firenvim,
