c43cd155c829d3b9b0832f054fb2fce55460d379
David <david.programming@protonmail.com>
2023-03-21 11:15:35 +0100

    add smart-splits.nvim mappings to swap windows

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -1,6 +1,11 @@
 return {
   "mrjones2014/smart-splits.nvim",
   config = function()
+    -- =============== SETUP ===============
+    require("smart-splits").setup({
+      cursor_follows_swapped_bufs = true,
+    })
+
     -- =============== MAPPINGS ===============
     -- resizing splits
     vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
@@ -13,5 +18,11 @@ return {
     vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
     vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
     vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+
+    -- swapping buffers between windows
+    vim.keymap.set("n", "<A-H>", require("smart-splits").swap_buf_left)
+    vim.keymap.set("n", "<A-J>", require("smart-splits").swap_buf_down)
+    vim.keymap.set("n", "<A-K>", require("smart-splits").swap_buf_up)
+    vim.keymap.set("n", "<A-L>", require("smart-splits").swap_buf_right)
   end,
 }
