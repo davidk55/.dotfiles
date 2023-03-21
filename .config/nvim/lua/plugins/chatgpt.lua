8425f535b95b46986f323a732270f038d7ca9083
David <david.programming@protonmail.com>
2023-03-21 11:13:59 +0100

    change chatgpt.nvim mappings

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -7,6 +7,15 @@ return {
   },
   config = function()
     -- =============== SETUP ===============
-    require("chatgpt").setup()
+    require("chatgpt").setup({
+      keymaps = {
+        close = { "<C-q>" },
+        submit = "<Enter>",
+        yank_last_code = "<C-c>",
+      },
+    })
+    -- =============== MAPPINGS ===============
+    vim.keymap.set("n", "<leader><leader>g", ":ChatGPT<CR>", { noremap = true, silent = true })
+    vim.keymap.set("v", "<leader><leader>g", ":<C-u>ChatGPTEditWithInstructions<CR>", { noremap = true, silent = true })
   end,
 }
