aa7a5786cc68e071d8b2f736c30a85f106e99dbb
David <david.programming@protonmail.com>
2023-03-25 20:09:29 +0100

    use legacy syntax highlighting for latex files

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -1,12 +1,10 @@
--- =============== OPTIONAS ===============
+-- =============== OPTIONS ===============
+-- general
 vim.bo.softtabstop = 4
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
+-- vimtex
 vim.g.vimtex_view_method = "sioyek"
 vim.g.tex_flavor = "latex" -- make `.tex` files indentified as LaTeX by default
-vim.g.vimtex_syntax_enabled = 0 -- disable message `using treesitter syntax`
+vim.g.maplocalleader = " "
 vim.opt.cmdheight = 2 -- remove `Hit ENTER to continue...` prompt
-
--- =============== MAPPINGS ===============
-vim.keymap.set("n", "<leader>l", ":VimtexCompile<CR>", { noremap = true, buffer = true, silent = true })
-vim.keymap.set("n", "<leader>c", ":VimtexClean<CR>", { noremap = true, buffer = true, silent = true })
