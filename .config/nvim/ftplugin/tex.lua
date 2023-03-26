bdbe5521fedfd630de508bf2afdf3dfd051d5500
David <david.programming@protonmail.com>
2023-03-26 15:35:39 +0200

    fix random highlight in sioyek on vimtex compile

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -4,7 +4,7 @@ vim.bo.softtabstop = 4
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 -- vimtex
-vim.g.vimtex_view_method = "sioyek"
+vim.g.vimtex_general_viewer = "sioyek"
 vim.g.tex_flavor = "latex" -- make `.tex` files indentified as LaTeX by default
 vim.g.maplocalleader = " "
 vim.opt.cmdheight = 2 -- remove `Hit ENTER to continue...` prompt
