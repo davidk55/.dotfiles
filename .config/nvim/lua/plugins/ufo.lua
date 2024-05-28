d7ab00742fc3a7c51ed8db02a5c0773df6009985
David <david.programming@protonmail.com>
2024-05-29 00:10:42 +0200

    adjust to new update

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -34,7 +34,11 @@ return {
     -- =============== SETUP ===============
     require("ufo").setup({
       open_fold_hl_timeout = 150,
-      close_fold_kinds = { "imports", "comment" },
+      close_fold_kinds_for_ft = {
+        default = { "imports", "comment" },
+        json = { "array" },
+        c = { "comment", "region" },
+      },
       preview = {
         win_config = {
           border = { "", "-", "", "", "", "-", "", "" },
