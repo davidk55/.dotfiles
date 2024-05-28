62ee73eb6a673a8b981ee9c69f6929d782840b97
David <david.programming@protonmail.com>
2024-05-29 00:23:37 +0200

    disable scope highlight

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,17 +1,9 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   config = function()
-    -- =============== VARIABLES ===============
-    local highlight = {
-      "BlankLineColor",
-    }
-    local hooks = require("ibl.hooks")
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "BlankLineColor", { fg = "#d3869b" })
-    end)
     -- =============== SETUP ===============
     require("ibl").setup({
-      scope = { highlight = highlight },
+      scope = { enabled = false },
     })
   end,
 }
