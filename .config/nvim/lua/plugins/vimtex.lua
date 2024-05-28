17c792e130dfb5ea9f02b0710b2534c9c7268040
David <david.programming@protonmail.com>
2024-05-29 00:08:29 +0200

    change vimtex defaults

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,4 +1,15 @@
 return {
   "lervag/vimtex",
-  ft = { "tex" },
+  init = function()
+    -- =============== VARIABLES ===============
+    -- general
+    vim.bo.softtabstop = 4
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    -- vimtex
+    vim.g.vimtex_general_viewer = "sioyek"
+    vim.g.vimtex_view_method = "sioyek"
+    vim.g.tex_flavor = "latex" -- make `.tex` files indentified as LaTeX by default
+    vim.opt.cmdheight = 2 -- remove `Hit ENTER to continue...` prompt
+  end,
 }
