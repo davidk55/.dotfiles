701ebc1b1b3d8ae3b7967e63985d78478b7c4905
David <david.programming@protonmail.com>
2024-05-29 00:21:50 +0200

    fix typo

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -83,7 +83,7 @@ return {
         formatting = {
           format = function(entry, vim_item)
             -- Kind icons
-            vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
             -- Source
             vim_item.menu = ({
               buffer = "[Buffer]",
