0dc7f26384f244af6926866edd796e69fa1abfc3
David <david.programming@protonmail.com>
2024-06-05 04:34:55 +0200

    make catppuccin work better for different plugins

--- .config/nvim/lua/plugins/themes/init.lua
+++ .config/nvim/lua/plugins/themes/init.lua
@@ -22,16 +22,60 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "frappe", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "frappe",
+        },
         integrations = {
+          barbar = true,
+          fidget = true,
+          flash = true,
           cmp = true,
+          dap = true,
+          dap_ui = true,
           gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = true,
+          illuminate = {
+            enabled = true,
+            lsp = true,
+          },
+          lsp_saga = true,
+          lsp_trouble = true,
+          mason = true,
           mini = {
             enabled = true,
-            indentscope_color = "",
           },
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+              ok = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+              ok = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          neotest = true,
+          noice = true,
+          notify = true,
+          nvimtree = true,
+          telescope = {
+            enabled = true,
+          },
+          treesitter = true,
+          treesitter_context = true,
+          ufo = true,
+          rainbow_delimiters = true,
+          which_key = false,
         },
       })
       vim.cmd([[au TextYankPost * silent! lua vim.highlight.on_yank{ higroup='Visual', timeout=300 }]])
