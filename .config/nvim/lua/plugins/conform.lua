695cbec661ba6aa7f001b2eccc6b247596071f38
David <david.programming@protonmail.com>
2024-06-23 12:33:23 +0200

    adjust option to plugin changes

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -57,7 +57,7 @@ return {
         if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
           return
         end
-        return { timeout_ms = 2000, lsp_fallback = true }
+        return { timeout_ms = 2000, lsp_format = "fallback" }
       end,
     })
 
