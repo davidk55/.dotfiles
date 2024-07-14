96e1254cd54355dfe5bc46e1275477f87c1b3c57
David <david.programming@protonmail.com>
2024-07-14 11:45:03 +0200

    fix typo

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -5,5 +5,5 @@ vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 -- vimtex
 vim.g.vimtex_general_viewer = "sioyek"
-vim.g.tex_flavor = "latex" -- make `.tex` files indentified as LaTeX by default
+vim.g.tex_flavor = "latex" -- make `.tex` files identified as LaTeX by default
 vim.opt.cmdheight = 2 -- remove `Hit ENTER to continue...` prompt
