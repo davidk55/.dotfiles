8d93ded9fd21417c3754198f732ffec8fc6829fb
David <david.programming@protonmail.com>
2025-09-07 15:38:05 +0200

    update colors from lsp results

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -5,7 +5,11 @@ return {
     require("colorizer").setup({
       user_default_options = {
         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        tailwind = true, -- Enable tailwind colors
+        tailwind = "both", -- Enable tailwind colors
+        tailwind_opts = { -- Options for highlighting tailwind names
+          update_names = true, -- When using tailwind = 'both', update tailwind names from LSP results.  See tailwind section
+        },
+        sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
       },
     })
   end,
