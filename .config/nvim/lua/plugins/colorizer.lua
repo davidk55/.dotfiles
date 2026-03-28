0b3375f68da27253e347a4adf43f0068a9ea36e7
David <david.programming@protonmail.com>
2026-03-28 19:27:05 +0100

    adjust to new syntax

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -3,13 +3,33 @@ return {
   config = function()
     -- =============== SETUP ===============
     require("colorizer").setup({
-      user_default_options = {
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        tailwind = "both", -- Enable tailwind colors
-        tailwind_opts = { -- Options for highlighting tailwind names
-          update_names = true, -- When using tailwind = 'both', update tailwind names from LSP results.  See tailwind section
+      options = {
+        parsers = {
+          hex = {
+            rrggbbaa = true, -- #RRGGBBAA (8-digit)
+          },
+          rgb = { enable = true }, -- rgb()/rgba() functions
+          hsl = { enable = true }, -- hsl()/hsla() functions
+          oklch = { enable = true }, -- oklch() function
+          hwb = { enable = true }, -- hwb() function (CSS Color Level 4)
+          lab = { enable = true }, -- lab() function (CIE Lab)
+          lch = { enable = true }, -- lch() function (CIE LCH)
+          css_color = { enable = true }, -- color() function (srgb, display-p3, a98-rgb, etc.)
+          tailwind = {
+            enable = true,
+            update_names = true,
+            lsp = {
+              enable = true,
+            },
+          },
+          sass = {
+            enable = true, -- parse Sass color variables
+          },
+          css_var_rgb = { enable = true }, -- CSS vars with R,G,B (e.g. --color: 240,198,198)
+          css_var = {
+            enable = true, -- resolve var(--name) references to their defined color
+          },
         },
-        sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
       },
     })
   end,
