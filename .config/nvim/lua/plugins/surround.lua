5258193ff91594be02c57a5260df300f5f246f88
David <david.programming@protonmail.com>
2026-03-28 19:30:03 +0100

    adjust default keybindings

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -11,5 +11,21 @@ return {
         },
       },
     })
+
+    -- See `:h nvim-surround.options`
+    vim.g.nvim_surround_no_normal_mappings = true
+    -- See `:h nvim-surround.keymaps`
+    vim.keymap.set("n", "sa", "<Plug>(nvim-surround-normal)", {
+      desc = "Add a surrounding pair around a motion (normal mode)",
+    })
+    vim.keymap.set("n", "sd", "<Plug>(nvim-surround-delete)", {
+      desc = "Delete a surrounding pair",
+    })
+    vim.keymap.set("n", "sr", "<Plug>(nvim-surround-change)", {
+      desc = "Change a surrounding pair",
+    })
+    vim.keymap.set("x", "sa", "<Plug>(nvim-surround-visual)", {
+      desc = "Add surrounding pair to selection",
+    })
   end,
 }
