3ed7a208868a4abc66193c4684a8830441e755ed
David <david.programming@protonmail.com>
2026-03-28 19:30:46 +0100

    add augroup to load mappings on lspattach

--- .config/nvim/lua/augroups.lua
+++ .config/nvim/lua/augroups.lua
@@ -21,3 +21,24 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end, { noremap = true, silent = true, buffer = true, desc = "Show Crate Documentation" })
   end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local buf = args.buf
+    local map = function(mode, lhs, rhs)
+      vim.keymap.set(mode, lhs, rhs, { buffer = buf })
+    end
+
+    map("n", "gd", vim.lsp.buf.definition)
+    map("n", "gr", vim.lsp.buf.references)
+    map("n", "K", vim.lsp.buf.hover)
+    map("n", "<leader>rn", vim.lsp.buf.rename)
+    map("n", "<leader>ca", vim.lsp.buf.code_action)
+
+    -- disable formatting for specific servers
+    if client.name == "lua_ls" then
+      client.server_capabilities.documentFormattingProvider = false
+    end
+  end,
+})
